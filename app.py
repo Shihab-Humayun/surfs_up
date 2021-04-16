1ffd95c07654070e61c2fbe424771c478029f808
Shihab Humayun <shihab_humayun@yahoo.com>
2021-04-16 16:14:48 -0400

    Created Routes.

--- app.py
+++ app.py
@@ -1,10 +1,111 @@
-from flask import Flask
+# PRACTICE
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello world'
+# @app.route('/')
+# def hello_world():
+#    return 'Hello world'
 
+# This is how you run the Flask application
 # set FLASK_APP=app.py
 # flask run
+
+#  If we wanted to import our app.py file into another Python file named example.py, the variable __name__ would be set to example. Here's an example of what that might look like:
+# import app
+
+# print("example __name__ = %s", __name__)
+
+# if __name__ == "__main__":
+#     print("example is being run directly.")
+# else:
+#     print("example is being imported")
+
+# Imported all dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+# Set up databaase
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# Allows us to access and query our SQLite database file
+Base = automap_base()
+
+# Reflects the database
+Base.prepare(engine, reflect=True)
+
+# References
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create Session Link
+session = Session(engine)
+
+# Set up Flask
+app = Flask(__name__)
+
+# Welcome Route
+@app.route("/")
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+# Precipitaion Analysis Route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+      filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+# Station Route
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+# Temperature Observation Route
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+# Start and Ending Date Route
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
